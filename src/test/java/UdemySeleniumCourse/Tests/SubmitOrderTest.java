accc5cbbdab8b9ab673c430b5bb472b09c2204b6
strdos <81235866+strdos@users.noreply.github.com>
2023-03-21 22:36:57 +0100

    add data sets via @DataProvider

--- src/test/java/UdemySeleniumCourse/Tests/SubmitOrderTest.java
+++ src/test/java/UdemySeleniumCourse/Tests/SubmitOrderTest.java
@@ -1,10 +1,10 @@
 package UdemySeleniumCourse.Tests;
 
-import UdemySeleniumCourse.AbstractComponents.AbstractComponent;
 import UdemySeleniumCourse.TestComponents.BaseTest;
 import UdemySeleniumCourse.pageobjects.*;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -16,8 +16,8 @@ public class SubmitOrderTest extends BaseTest {
     String itemToAdd = "ZARA COAT 3";
     String country = "Australia";
     String confirmMsgText = "THANKYOU FOR THE ORDER.";
-    @Test
-    public void submitOrder() throws IOException {
+    @Test(dataProvider = "getData", groups = {"Purchase"})
+    public void submitOrder(String email, String password, String itemToAdd) throws IOException {
         ProductCatalog productCatalog = landingPage.loginToApplication(email, password);
         List<WebElement> products = productCatalog.getProductList();
         productCatalog.addProductToCart(itemToAdd);
@@ -36,5 +36,9 @@ public class SubmitOrderTest extends BaseTest {
         ProductCatalog productCatalog = landingPage.loginToApplication(email, password);
         OrderPage orderPage = productCatalog.goToOrdersPage();
         Assert.assertTrue(orderPage.isAddedProductDisplayed(itemToAdd));
+    }
+    @DataProvider
+    public Object[][] getData() {
+        return new Object[][] { {email, password, itemToAdd}, {"alex2@test.com", "Test1234!2", "ADIDAS ORIGINAL"} };
     }
 }
